ea454d2a7ab23f02c60769d4d3c8d2ab1e65716b
Santu Mahapatra <santu.m@multunus.com>
2014-11-14 11:31:34 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nov14Spike::Application.config.secret_key_base = '37230821c7e0c3bfd953d5c3e9464c9962ac51a6358d743678176acac5711c24ae56c11a96803ebc50692641c6def38203c7ddadd8366e4ddcca76449ec827a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nov14Spike::Application.config.session_store :cookie_store, key: '_Nov_14_Spike_session'
